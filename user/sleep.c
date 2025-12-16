fe38361fa4b15a1a19aa5e809689893fe429be5e
Limics <2311605992@qq.com>
2025-12-16 18:46:39 +0800

    ex1 all

--- user/sleep.c
+++ user/sleep.c
@@ -4,18 +4,11 @@
 
 int main(int argc, char *argv[])
 {
-    // 检查参数数量
     if (argc != 2) {
         fprintf(2, "Usage: sleep ticks\n");
         exit(1);
     }
-
-    // 将字符串参数转换为整数
     int ticks = atoi(argv[1]);
-    
-    // 调用 sleep 系统调用
     sleep(ticks);
-    
-    // 程序执行完毕，正常退出
     exit(0);
 }
