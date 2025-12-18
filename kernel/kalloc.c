50d6f76079c5244333ebcc2bd67918d7ed9e560c
Limics <2311605992@qq.com>
2025-12-18 09:47:15 +0800

    lab 8 new

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,7 +9,9 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define STEAL_BATCH 256
 void freerange(void *pa_start, void *pa_end);
+static void steal(int me);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -21,12 +23,30 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+} kmem[NCPU];
+
+static uchar page_owner[(PHYSTOP - KERNBASE) / PGSIZE];
+static inline int
+pageindex(void *pa)
+{
+  return ((uint64)pa - KERNBASE) / PGSIZE;
+}
+
+static void
+set_owner_list(struct run *p, int owner)
+{
+  for(; p; p = p->next){
+    page_owner[pageindex((void*)p)] = owner;
+  }
+}
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++){
+    initlock(&kmem[i].lock, "kmem");
+    kmem[i].freelist = 0;
+  }
   freerange(end, (void*)PHYSTOP);
 }
 
@@ -50,33 +70,99 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+  if((uint64)pa < KERNBASE)
+    panic("kfree: pa < KERNBASE");
 
-  // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
-
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
-}
+  int idx = pageindex(pa);
+  int id = page_owner[idx];
+  if(id < 0 || id >= NCPU) id = 0;
 
+  acquire(&kmem[id].lock);
+  r->next = kmem[id].freelist;
+  kmem[id].freelist = r;
+  release(&kmem[id].lock);
+}
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
 void *
 kalloc(void)
 {
-  struct run *r;
+  struct run *r = 0;
+  int id;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
+  push_off();
+  id = cpuid();
+  acquire(&kmem[id].lock);
+  r = kmem[id].freelist;
   if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    kmem[id].freelist = r->next;
+  release(&kmem[id].lock);
+  pop_off();
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
+  if(r == 0){
+    steal(id);
+
+    push_off();
+    acquire(&kmem[id].lock);
+    r = kmem[id].freelist;
+    if(r)
+      kmem[id].freelist = r->next;
+    release(&kmem[id].lock);
+    pop_off();
+  }
+
+  if(r){
+    page_owner[pageindex((void*)r)] = id;
+    memset((char*)r, 5, PGSIZE);
+  }
   return (void*)r;
+}
+
+static void
+steal(int me)
+{
+  for(int off = 1; off < NCPU; off++){
+    int i = (me + off) % NCPU;
+
+    acquire(&kmem[i].lock);
+    struct run *head = kmem[i].freelist;
+    if(head == 0){
+      release(&kmem[i].lock);
+      continue;
+    }
+
+    struct run *slow = head;
+    struct run *fast = head;
+    struct run *prev = 0;
+
+    while(fast && fast->next){
+      prev = slow;
+      slow = slow->next;
+      fast = fast->next->next;
+    }
+
+    struct run *stolen = head;
+    struct run *remain = slow;
+
+    if(prev){
+      prev->next = 0;          // prev 是 stolen 的尾巴
+      kmem[i].freelist = remain;
+    } else {
+      kmem[i].freelist = 0;    // 只有 1 个节点
+      prev = stolen;           // 让 prev 也指向尾巴
+    }
+    release(&kmem[i].lock);
+
+    set_owner_list(stolen, me);
+
+    acquire(&kmem[me].lock);
+    prev->next = kmem[me].freelist;  // O(1) 拼接
+    kmem[me].freelist = stolen;
+    release(&kmem[me].lock);
+    return;
+  }
 }
